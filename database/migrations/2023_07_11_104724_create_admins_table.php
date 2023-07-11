c883373fd0c7e92f5130002c6589f7419c47f646
foxdevuz <abdullajonovboburjon05@gmail.com>
2023-07-11 19:07:44 +0500

    optimized

--- database/migrations/2023_07_11_104724_create_admins_table.php
+++ database/migrations/2023_07_11_104724_create_admins_table.php
@@ -3,9 +3,7 @@
 use App\Models\Admins;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Str;
 return new class extends Migration
 {
     /**
